b6d94df0e04c60bb041a0c0ab23f3771fed4d045
Masayuki <soy2.san@gmail.com>
2021-03-12 20:03:45 +0900

    初めのコミット＆Colors

---
+++ Steps/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  Steps
+//
+//  Created by 山口雅之 on 2021/03/12.
+//
+
+import UIKit
+
+struct Colors {
+    let limeGreen = UIColor(red: 88/255, green: 219/255, blue: 176/255, alpha: 0.86)
+    let paleGreen = UIColor(red: 145/225, green: 194/255, blue: 76/255, alpha: 0.76)
+    let darkGreen = UIColor(red: 50/255, green: 66/255, blue: 61/255, alpha: 0.26)
+    let white = UIColor.systemGray2
+    let black = UIColor(red: 20/255, green: 20/255, blue: 30/255, alpha: 0.8)
+}
